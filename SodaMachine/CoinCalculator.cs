30b1397243523a87768f01272329b6573ce987e5
TimH <tmherron09@gmail.com>
2020-07-21 16:58:59 -0500

    Add Utility Class CoinCalculator as intermediate for coin calculations
    Added CoinCalculator as a static helper class. Unsure if better practice to put these static methods on the Coin class itself. May refactor later.

---
+++ SodaMachine/CoinCalculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    public static class CoinCalculator
+    {
+
+        /// <summary>
+        /// Get the value of a list of coins.
+        /// </summary>
+        /// <param name="coins">List of coins you want the total value.</param>
+        /// <returns>The value of all the coins in the list.</returns>
+        public static double GetValueOfCoins(List<Coin> coins)
+        {
+            double totalValue = 0;
+            foreach(Coin coin in coins)
+            {
+                totalValue += coin.Value;
+            }
+            return totalValue;
+        }
+
+
+
+
+
+
+        #region Depreciated/ Unused
+
+        // Redundant switch case.
+        //public static double GetValueOfCoins(List<Coin> coins)
+        //{
+        //    double totalValue = 0;
+        //    foreach (Coin coin in coins)
+        //    {
+        //        switch (coin.name)
+        //        {
+        //            case "quarter":
+        //                totalValue += coin.Value;
+        //                break;
+        //            case "dime":
+        //                totalValue += coin.Value;
+        //                break;
+        //            case "nickel":
+        //                totalValue += coin.Value;
+        //                break;
+        //            case "penny":
+        //                totalValue += coin.Value;
+        //                break;
+        //            default:
+        //                break;
+        //        }
+        //    }
+        //    return totalValue;
+        //}
+
+        #endregion
+
+    }
+}
