a0e574186afaf545061ca05a11d90422fab5b4c7
TimH <tmherron09@gmail.com>
2020-07-21 10:40:43 -0500

    Outline for Simulation and Soda machine comments

--- SodaMachine/Simulation.cs
+++ SodaMachine/Simulation.cs
@@ -8,5 +8,27 @@ namespace SodaMachine
 {
     class Simulation
     {
+        SodaMachine sodaMachine;
+        Customer customer;
+
+        public Simulation()
+        {
+            customer = new Customer();
+            sodaMachine = new SodaMachine();
+        }
+
+        // RunSimulation()
+        // Call the user interface for Customer and Soda Machine Interaction.
+        // Go between for Customer and SodaMachine class
+        // Call to ask for which soda the user wants.
+        // Ask soda machine if soda is left. if true continue, if not go back to start.
+        // Call to ask user to choose how much change they want to insert.
+        // Ask soda machine if enough change is inserted. if false, ask for coin input again.
+        // Ask soda machine if enough coin is available for giving change. If false, inform customer. Ask if new soda or new insert change?
+        // If enough change and coin return, Get coin from customer and give coin to soda machine.
+        // Recieve change from soda machine and give to Customer.
+        // Recieve soda and give soda to customer to put in backpack.
+        // Start over.
+
     }
 }
--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -8,5 +8,26 @@ namespace SodaMachine
 {
     class SodaMachine
     {
+        List<Coin> register;
+        List<Can> inventory;
+
+        // On construct define starting coins
+        public SodaMachine()
+        {
+
+        }
+
+        // Methods
+
+        // Check if can is available.
+        // Return the price of a can.
+        // Calculate if enough coins inserted.
+        // Calculate Change value
+        // Calculate if enough coins can be returned.
+        // If enough coins inserted and enough change, dispense soda and subtract from inventory
+        // Give change and subtract from register.
+
+
+
     }
 }
